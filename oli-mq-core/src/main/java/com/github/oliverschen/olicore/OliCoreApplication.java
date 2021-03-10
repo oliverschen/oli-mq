80c1036e6504ee6603e45c154d4710d1784ec36f
chenkui <jihe12180@gmail.com>
2021-03-10 23:58:50 +0800

    olimq first commit

---
+++ oli-mq-core/src/main/java/com/github/oliverschen/olicore/OliCoreApplication.java
@@ -0,0 +1,13 @@
+package com.github.oliverschen.olicore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OliCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OliCoreApplication.class, args);
+    }
+
+}
---
+++ oli-mq-core/src/test/java/com/github/oliverschen/olicore/OliCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.oliverschen.olicore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OliCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
