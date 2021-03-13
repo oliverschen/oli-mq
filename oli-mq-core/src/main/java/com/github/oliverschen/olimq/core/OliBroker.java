d40a65e027123f7933973348a72f75b16ad8f2e4
chenkui <jihe12180@gmail.com>
2021-03-13 18:00:30 +0800

    broker and queue

--- oli-mq-core/src/main/java/com/github/oliverschen/olimq/core/OliBroker.java
+++ oli-mq-core/src/main/java/com/github/oliverschen/olimq/core/OliBroker.java
@@ -2,11 +2,36 @@ package com.github.oliverschen.olimq.core;
 
 import lombok.Data;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 
 /**
  * broker 信息
+ * 1. 创建 topic
+ * 2. 缓存 topic 信息，一个 topic 中包含一个 队列，队列里面存放多条同类的消息
+ *
+ * @author chenkui
  */
 @Data
 public class OliBroker {
 
+    /**
+     * 队列的容量
+     */
+    private static final Integer CAPITAL = 1000;
+
+    /**
+     * topic 容器
+     */
+    private static ConcurrentHashMap<String, Object> topics = new ConcurrentHashMap<>(64);
+
+    /**
+     * 创建 topic
+     * @param topic 主题名称
+     * @return result
+     */
+    public boolean createTopic(String topic) {
+        topics.putIfAbsent(topic, new OliMq(topic, CAPITAL));
+        return true;
+    }
 }
--- oli-mq-core/src/main/java/com/github/oliverschen/olimq/core/OliMq.java
+++ oli-mq-core/src/main/java/com/github/oliverschen/olimq/core/OliMq.java
@@ -2,9 +2,32 @@ package com.github.oliverschen.olimq.core;
 
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * 阻塞队列
+ * 这个类的实例对象保存当前 topic 的数据
+ * @author chenkui
+ */
 @Data
+@NoArgsConstructor
 public class OliMq {
 
+    /**
+     * 绑定 topic
+     */
+    private String topic;
+
+    /**
+     * topic 阻塞队列
+     */
+    private LinkedBlockingQueue<Object> queue;
+
+    public OliMq(final String topic,final Integer capital) {
+        this.topic = topic;
+        this.queue = new LinkedBlockingQueue<>(capital);
+    }
 
 }
