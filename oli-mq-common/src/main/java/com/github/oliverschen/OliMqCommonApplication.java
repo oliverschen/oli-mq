b42cb7b38bc905f220c6ecf965835c5d8981b15c
chenkui <jihe12180@gmail.com>
2021-03-16 23:39:00 +0800

    add common module

---
+++ oli-mq-common/src/main/java/com/github/oliverschen/OliMqCommonApplication.java
@@ -0,0 +1,13 @@
+package com.github.oliverschen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OliMqCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OliMqCommonApplication.class, args);
+    }
+
+}
