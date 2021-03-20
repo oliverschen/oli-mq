72c75847386006b8236c6af3fb28deb99026f724
chenkui <jihe12180@gmail.com>
2021-03-20 16:05:13 +0800

    create

--- oli-mq-server/src/main/java/com/github/oliverschen/api/ServiceController.java
+++ oli-mq-server/src/main/java/com/github/oliverschen/api/ServiceController.java
@@ -1,5 +1,6 @@
 package com.github.oliverschen.api;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,14 @@ import static com.github.oliverschen.constant.SystemConstant.SYSTEM_NAME;
 @RestController
 @RequestMapping(SYSTEM_NAME)
 public class ServiceController {
+
+    /**
+     * 接收创建消息
+     */
+    @PostMapping
+    public void create() {
+
+    }
 
 
 
